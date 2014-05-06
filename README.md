0a2480579f978d055714b347298ebcf8d26ef2d7
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-05-06 20:13:00 +0300

    Initial code

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ nor-github
 ==========
 
 Github API library
+
+Create an issue
+---------------
+
+```javascript
+var github = require('./index.js');
+
+github.create_issue({
+	'auth_token': 'foobarsecret',
+	'user_agent': 'jheusala',
+	'repository': 'sendanor/nor-github',
+	'title': 'Test issue',
+	'body': 'Test content.'
+}).then(function() {
+	console.log('OK');
+}).fail(function(err) {
+	util.error('Error: ' +err);
+}).done();
+```
---
+++ src/index.js
@@ -0,0 +1,74 @@
+/** GitHub API actions as promise returning functions */
+
+var mod = module.exports = {};
+var debug = require('nor-debug');
+var is = require('nor-is');
+var request = require('nor-rest').request;
+var ref = require('nor-ref');
+
+/** Create an issue
+ * See https://developer.github.com/v3/issues/#create-an-issue
+ * @param opts.auth_token {string} Required. The authorization token
+ * @param opts.user_agent {string} Required. The user agent header. Use your GitHub username or application name.
+ * @param opts.repository {string} Required. The repository where to create the issue, including the owner (like `sendanor/foo`)
+ * @param opts.title {string} Required. The title of the issue.
+ * @param opts.body {string} The body of the issue.
+ * @param opts.milestone {number} The milestone to associate this issue.
+ * @param opts.labels {array of strings} Labels for the issue.
+ */
+mod.create_issue = function(opts) {
+	opts = opts || {};
+	debug.assert(opts).is('object');
+
+	// Check parameters
+	debug.assert(opts.repository).is('string');
+	debug.assert(opts.auth_token).ignore(undefined).is('string');
+	debug.assert(opts.user_agent).ignore(undefined).is('string');
+	debug.assert(opts.title).is('string');
+	debug.assert(opts.body).ignore(undefined).is('string');
+	debug.assert(opts.milestone).ignore(undefined).is('number');
+	debug.assert(opts.labels).ignore(undefined).is('array');
+
+	if(!opts.auth_token) {
+		throw new TypeError("opts.auth_token required!");
+	}
+
+	if(!opts.title) {
+		throw new TypeError("Missing required opts.title");
+	}
+
+	debug.log('opts = ', opts);
+
+	// Build body
+	var data = {};
+	data.title = opts.title;
+
+	if(is.defined(opts.body)) {
+		data.body = opts.body;
+	}
+
+	if(is.defined(opts.milestone)) {
+		data.milestone = opts.milestone;
+	}
+
+	if(is.defined(opts.labels)) {
+		data.labels = opts.labels;
+	}
+
+	// Create request
+	var url = 'https://api.github.com/repos/' + opts.repository + '/issues';
+	debug.log('POSTing to '+url+'...');
+	return request.json(url, {
+		'method': 'POST',
+		'headers': {
+			'User-Agent': opts.user_agent,
+			'Accept': 'application/vnd.github.v3+json',
+			'Content-Type': 'application/json;charset=utf8',
+			'Authorization': 'token '+opts.auth_token
+		},
+		'body': data
+	});
+	
+};
+
+/* EOF */
